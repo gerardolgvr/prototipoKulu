7ecfcbb4df26613acd45a7aff48417d2bfb2fd5e
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2017-08-18 18:24:51 -0500

    Adding bootstrap 4, and some components

--- client/main.js
+++ client/main.js
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom';
 // Importing routes
 import { routes } from '../imports/routes/routes';
 
+// Import Tether.js - Bootstrap tooltip requires Tether.
+// We also add 'global.Tether' as a workaround for Meteor.
+import tether from 'tether';
+global.Tether = tether;
+// Import Bootstrap js npm module.
+bootstrap = require('bootstrap');
+
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
 });
